9acccecb8560924dcf07e72eddef5a66bad0e302
gongqingkui WinTabletPc <gongqingkui@126.com.winpc>
2016-07-11 14:27:33 +0800

    now the sounder distance is working

--- UltraSoundDistance/UltraSoundDistance.ino/UltraSoundDistance.ino.ino
+++ UltraSoundDistance/UltraSoundDistance.ino/UltraSoundDistance.ino.ino
@@ -1,26 +1,19 @@
-const int TrigPin = 2; 
-const int EchoPin = 3; 
-long cm; 
-void setup() 
-{ 
-Serial.begin(9600); 
-pinMode(TrigPin, OUTPUT); 
-pinMode(EchoPin, INPUT); 
-} 
-void loop() 
-{ 
-//发一个10ms的高脉冲去触发TrigPin 
-digitalWrite(TrigPin, LOW); 
-delayMicroseconds(2); 
-digitalWrite(TrigPin, HIGH); 
-delayMicroseconds(10); 
-digitalWrite(TrigPin, LOW); 
- 
-//cm = pulseIn(EchoPin, HIGH) / 58.0; //算成厘米 
-cm = pulseIn(EchoPin,HIGH)/1000*340/2;
-cm = (int(cm * 100.0)) / 100.0; //保留两位小数 
-Serial.print(cm); 
-Serial.print("cm"); 
-Serial.println(); 
-delay(1000); 
+int inputPin = 4;//echo
+int outputPin = 5;//trig 
+void setup(){
+  Serial.begin(9600);
+  pinMode(inputPin,INPUT);
+  pinMode(outputPin,OUTPUT);
 }
+void loop(){
+  digitalWrite(outputPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(outputPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(outputPin,LOW);
+  float distance = pulseIn(inputPin,HIGH);
+  distance = distance / 58 ;
+  Serial.println(distance);
+  delay(1000); 
+  }
+
